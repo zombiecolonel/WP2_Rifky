c783168b1b7ca6319a308e685a3b72b1d099645b
zombiecolonel <redkeymon27@gmail.com>
2022-03-28 00:47:13 +0700

    first commit

---
+++ application/views/view-latihan.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Latihan 1</title>
+</head>
+
+<body>
+	Halo Kawan.. Kita belajar web programing!!<br>
+	Nilai 1 = <?= $nilai1; ?>
+	Nilai 2 = <?= $nilai2; ?>
+	ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>	
